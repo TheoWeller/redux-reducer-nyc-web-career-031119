fd281e7b0f6bf937a3229e1580eec54b79c80146
Theo Weller <weller.theodor@gmail.com>
2019-06-03 10:40:20 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
 export function manageFriends(state, action){
+  switch(action.type) {
+    case "ADD_FRIEND":
+
+      return {friends: [...state.friends, action.friend]}
+    case "REMOVE_FRIEND":
+      const updated = state.friends.filter(friend => {
+        return friend.id !== action.id
+      })
+      return {...state, friends: updated}
+    default:
+        return state;
+  }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,9 @@
 export function managePresents(state, action){
+  switch(action.type){
+    case "INCREASE":
+      const updatedPresents = state.numberOfPresents + 1
+      return {numberOfPresents: updatedPresents}
+    default:
+      return state;
+  }
 }
